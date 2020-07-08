49c7e176b2d866190a41bc961cc556e22b9b3c28
Hakob Hakobyan <hakobhakobyan1992@yahoo.com>
2020-07-08 22:44:56 +0400

    First commit

---
+++ precache-manifest.04a11d9a0207ed57454e43ed7120e95f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3730ae9922c31360fba7d33fb6a26a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467732c7a8982e2d0a72",
+    "url": "/static/css/main.4ef3b222.chunk.css"
+  },
+  {
+    "revision": "b31455eb71ad8acb06c7",
+    "url": "/static/js/2.5eef9fc3.chunk.js"
+  },
+  {
+    "revision": "bcf56c6ea3207d98f3a741dbe9ffc372",
+    "url": "/static/js/2.5eef9fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467732c7a8982e2d0a72",
+    "url": "/static/js/main.8a795c11.chunk.js"
+  },
+  {
+    "revision": "8a1b214b749b18912a79",
+    "url": "/static/js/runtime-main.873eb81f.js"
+  }
+]);
